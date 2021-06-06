b6c8df0f1f587a41c4feabcc7c7e1795a3599dc3
dvergara <dvergara@gildmund.com.ar>
2021-06-05 22:25:15 -0300

    Test Punto 1 terminado

---
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/SemillasTest.kt
@@ -0,0 +1,45 @@
+package ar.edu.unahur.obj2.semillasAlViento
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+
+class SemillaTest : DescribeSpec({
+    val menta = Menta(2020, 0.3f)
+    val mentaGrande = Menta(2020, 0.6f)
+
+    val sojaNormal = Soja(2019, 0.4f, false )
+    val sojaTransgenica = Soja(2019, 0.6f, true )
+
+    describe ("Plantas") {
+        describe ("Horas de sol que tolera") {
+            it ("Menta") {
+                menta.horasDeSolQueTolera().shouldBe(6)
+            }
+            it ("Soja") {
+                sojaNormal.horasDeSolQueTolera().shouldBe(6)
+                sojaTransgenica.horasDeSolQueTolera().shouldBe(14)
+            }
+
+        }
+
+        describe ("Es fuerte?") {
+            it ("Menta") {
+                menta.esFuerte().shouldBeFalse()
+            }
+            it ("Soja") {
+                sojaNormal.esFuerte().shouldBeFalse()
+                sojaTransgenica.esFuerte().shouldBeTrue()
+            }
+        }
+
+        describe ("Da semillas") {
+            it ("Menta") {
+                menta.daSemillas().shouldBeFalse()
+                mentaGrande.daSemillas().shouldBeTrue()
+            }
+        }
+
+    }
+})
